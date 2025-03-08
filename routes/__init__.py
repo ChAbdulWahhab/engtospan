dd30e99224574e74f3b78e0e6ddfbb7a57868032
oliver-dev01 <oliver.james.dev@gmail.com>
2025-03-09 02:21:48 +0500

    Error fixed

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from .main import main_bp
+import os
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder=os.path.abspath('templates'))
     app.register_blueprint(main_bp)
     return app
